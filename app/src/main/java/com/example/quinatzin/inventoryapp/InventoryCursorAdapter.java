1c77575361c78d6bf1956db2ddddad9c2a95c754
Keynotseen89 <ksintora@yahoo.com>
2018-01-05 14:51:20 -0800

    Inventory Cursor Adapter used to display database in the ListView.

---
+++ app/src/main/java/com/example/quinatzin/inventoryapp/InventoryCursorAdapter.java
@@ -0,0 +1,55 @@
+package com.example.quinatzin.inventoryapp;
+
+/**
+ * Created by Quinatzin Sintora on 1/4/2018.
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.quinatzin.inventoryapp.data.InventoryContract.InventoryEntry;
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    /**
+     * @param context
+     * @param cursor
+     */
+    public InventoryCursorAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0 /* flags */);
+    }
+
+    /**
+     * @param context
+     * @param cursor
+     * @param parent
+     * @return
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find fields to populate in inflate template
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        //TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+
+        //Find the columns of pets attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_NAME);
+        //  int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_PRICE);
+
+        //Read the inventory attribute from the cursor for the current inventory
+        String itemName = cursor.getString(nameColumnIndex);
+        //Integer itemPrice = cursor.getInt(priceColumnIndex);
+
+        nameTextView.setText(itemName);
+        // summaryTextView.setText(itemPrice);
+    }
+}
