d923a4e0b38c45ee21b7953fe5e822e906517cf5
Keynotseen89 <ksintora@yahoo.com>
2018-02-23 21:02:05 -0800

    quantityTextView
    added quantityTextView to be displayed in the ListView

--- app/src/main/java/com/example/quinatzin/inventoryapp/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/quinatzin/inventoryapp/InventoryCursorAdapter.java
@@ -9,6 +9,7 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
@@ -38,18 +39,28 @@ public class InventoryCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // Find fields to populate in inflate template
-        TextView nameTextView = view.findViewById(R.id.name);
-        TextView summaryTextView = view.findViewById(R.id.summary);
+        TextView nameTextView, summaryTextView, quantityTextView;
+        Button saleButton = view.findViewById(R.id.buttonID);
+
+        nameTextView = view.findViewById(R.id.name);
+        summaryTextView = view.findViewById(R.id.summary);
+        quantityTextView = view.findViewById(R.id.quantity_summary);
 
         //Find the columns of pets attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_NAME);
         int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_QUANTITY);
 
         //Read the inventory attribute from the cursor for the current inventory
         String itemName = cursor.getString(nameColumnIndex);
         Integer itemPrice = cursor.getInt(priceColumnIndex);
         String itemPriceValue = Integer.toString(itemPrice);
-        nameTextView.setText(itemName);
-        summaryTextView.setText(itemPriceValue);
+
+        Integer itemQuantity = cursor.getInt(quantityColumnIndex);
+        String itemQuantityValue = Integer.toString(itemQuantity);
+
+        nameTextView.setText(itemName + " :");
+        summaryTextView.setText(" $" + itemPriceValue);
+        quantityTextView.setText(", Quantity:" + itemQuantityValue);
     }
 }
